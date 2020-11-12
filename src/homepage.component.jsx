ee891fc758f5f57f64f74310e84cb8ee527dbcb2
joseph <josephjoy07@gmail.com>
2020-11-12 15:52:45 +0530

    lesson 1

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import "./homepage.styles.scss";
+
+export const HomePage = () => (
+  <div className="homepage">
+    <div className="menu">
+      <div className="card">
+        <div className="card__content-box">
+          <h1 className="card__title">hats</h1>
+          <span className="card__subtitle">shop now!</span>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card__content-box">
+          <h1 className="card__title">hats</h1>
+          <span className="card__subtitle">shop now!</span>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card__content-box">
+          <h1 className="card__title">hats</h1>
+          <span className="card__subtitle">shop now!</span>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card__content-box">
+          <h1 className="card__title">hats</h1>
+          <span className="card__subtitle">shop now!</span>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="card__content-box">
+          <h1 className="card__title">hats</h1>
+          <span className="card__subtitle">shop now!</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
